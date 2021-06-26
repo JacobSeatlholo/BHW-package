50f476f23275eed0116e8481ac42d25d9904ebde
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-06-26 19:31:51 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Business Hustle.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Simple eternity holdings (Pty) Ltd.`,
     },
   },
   presets: [
