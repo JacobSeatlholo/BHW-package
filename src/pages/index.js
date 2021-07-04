4466c317f6b79888ad4cf2f58170508512be2499
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-07-04 17:02:25 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,9 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
-const features = [
+const features = 
   {
+    
     title: <>Easy to Use Task Management App</>,
      imageUrl: 'img/undraw_docusaurus_mountain.svg',
      description: (
