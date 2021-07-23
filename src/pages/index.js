0ad69f4a77782bc8349057b6e8166e0d6a85452e
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-07-23 21:53:17 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ import React from 'react';
      imageUrl: 'img/undraw_docusaurus_react.svg',
      description: (
        <>
-         Get a fully stacked team and tackle your organisations end to end design needs.
+         We are here to provide you with innovative and affordable digital marketing and web development services.Get a fully stacked team and tackle your organisations end to end design needs.
        </>
      ),
    },
