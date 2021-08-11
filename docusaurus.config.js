289e7893d06a42404837bb3189f9537f6e11f239
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-08-11 07:27:00 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/JacobSeatlholo/BHW-package',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
